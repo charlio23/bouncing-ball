b6b14953585488cde3d33c1bbde8cc5d6473c2ee
Carles Balsells Rodas <carlesbalsellsrodas@hotmail.com>
2022-03-11 10:54:59 +0000

    mse through time implemented

--- trial_VRNN.py
+++ trial_VRNN.py
@@ -1,9 +1,10 @@
 from curses.ascii import DEL
+import re
 import torch
 from torch.utils.data import DataLoader
 from models.VRNN import VRNN
 from dataloaders.bouncing_data import BouncingBallDataLoader
-from utils.losses import nll_gaussian, kld_loss
+from utils.losses import nll_gaussian, kld_loss, mse_through_time
 
 
 
@@ -17,9 +18,13 @@ vrnn = VRNN(input_dim, 128, 32).float()
 print(vrnn)
 reconstr_seq, z_params, x_params = vrnn(sample)
 print(z_params.size())
-print(x_params.size())
-print(reconstr_seq[0])
+#print(x_params.size())
+#print(reconstr_seq[0])
 print(kld_loss(z_params[:,:,0,:], z_params[:,:,1,:]))
-print(nll_gaussian(x_params[:,:,0,:], x_params[:,:,1,:], sample))
-loss = kld_loss(z_params[:,:,0,:], z_params[:,:,1,:]) + nll_gaussian(x_params[:,:,0,:], x_params[:,:,1,:], sample)
-print(loss)
+#print(nll_gaussian(x_params[:,:,0,:], x_params[:,:,1,:], sample))
+#loss = kld_loss(z_params[:,:,0,:], z_params[:,:,1,:]) + nll_gaussian(x_params[:,:,0,:], x_params[:,:,1,:], sample)
+#print(loss)
+
+mse_t_t = mse_through_time(reconstr_seq, sample, False)
+print(mse_t_t)
+print(mse_t_t.size())
