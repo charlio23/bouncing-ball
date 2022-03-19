c8ba97fb51d4ad15f9f3232019d44054a4d22bad
Carles Balsells <carlesbalsellsrodas@hotmail.com>
2022-03-19 10:53:00 +0000

    VRNN with multi LSTM layer

--- models/VRNN.py
+++ models/VRNN.py
@@ -1,28 +1,37 @@
+from re import X
 import torch
 import torch.nn as nn
 from models.modules import MLP, CNNEncoder, CNNEncoderPosition, CNNResidualDecoder
 
 class VRNN(nn.Module):
-    def __init__(self, input_dim, input_pos, hidden_dim, latent_dim, input_type='base', decoder='LSTM'):
+    def __init__(self, input_dim, input_pos, hidden_dim, latent_dim, num_rec_layers=1, input_type='base', decoder='LSTM'):
         super(VRNN, self).__init__()
         self.input_dim = input_dim
         self.input_pos = input_pos
         self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
+        self.num_rec_layers = num_rec_layers
         self.prior = MLP(self.hidden_dim, self.hidden_dim, self.latent_dim*2)
         self.input_type = input_type
         if input_type=='visual':
-            self.embedder_x = CNNEncoder(self.input_dim, self.hidden_dim, 4)
+            self.embedder_x = CNNEncoder(self.input_dim, self.latent_dim, 4)
             self.decoder = CNNResidualDecoder()
         else:
-            self.embedder_x = CNNEncoderPosition(self.input_dim, input_pos, self.hidden_dim, 4)
-            self.decoder = MLP(self.hidden_dim*2, self.hidden_dim, input_pos)
-        self.encoder = MLP(self.hidden_dim*2, self.hidden_dim, self.latent_dim*2)
-        self.embedder_z = MLP(self.latent_dim, self.hidden_dim, self.hidden_dim)
+            #self.embedder_x = CNNEncoderPosition(self.input_dim, input_pos, self.hidden_dim, 4)
+            self.embedder_x = MLP(self.input_pos, self.hidden_dim, self.latent_dim)
+            self.decoder = MLP(self.hidden_dim + self.latent_dim, self.hidden_dim, self.input_pos)
+        self.encoder = MLP(self.hidden_dim + self.latent_dim, self.hidden_dim, self.latent_dim*2)
+        self.embedder_z = MLP(self.latent_dim, self.hidden_dim, self.latent_dim)
         if decoder=='vainilla':
             self.hidden_decoder = None
         elif decoder=='LSTM':
-            self.hidden_decoder = nn.LSTMCell(self.hidden_dim*2, self.hidden_dim)
+            self.hidden_decoder = nn.LSTMCell(self.latent_dim*2, self.hidden_dim)
+            if num_rec_layers==3:
+                self.hidden_decoder = nn.ModuleList([
+                    nn.LSTMCell(self.latent_dim*2, self.hidden_dim),
+                    nn.LSTMCell(self.hidden_dim, self.hidden_dim),
+                    nn.LSTMCell(self.hidden_dim, self.hidden_dim)
+                ])
         elif decoder=='GRU':
             self.hidden_decoder = None
         else:
@@ -36,79 +45,99 @@ class VRNN(nn.Module):
                 nn.init.xavier_normal_(m.weight.data)
                 m.bias.data.fill_(0.1)
 
-    def _inference(self, x, pos, h):
-        encoder_in = torch.cat([self.embedder_x(x, pos), h], dim=-1)
+    def _inference(self, x, h):
+        if self.num_rec_layers == 1:
+            encoder_in = torch.cat([self.embedder_x(x), h], dim=-1)
+        else:
+            encoder_in = torch.cat([self.embedder_x(x), h[-1]], dim=-1)
         (z_mu, z_log_var) = self.encoder(encoder_in).split(self.latent_dim, dim=-1)
         eps = torch.normal(mean=torch.zeros_like(z_mu)).to(x.device)
-        z_std = torch.minimum((z_log_var*0.5).exp(), torch.FloatTensor([100.]).to(h.device))
+        z_std = torch.minimum((z_log_var*0.5).exp(), torch.FloatTensor([100.]).to(x.device))
         sample = z_mu + z_std*eps
         return sample, z_mu, z_log_var
 
-    def _decode(self, z, im, h_prev, c_prev=None):
+    def _decode(self, z, h_prev, c_prev=None):
         embed_z = self.embedder_z(z)
-        decoder_in = torch.cat([embed_z, h_prev], dim=-1)
-        pos = self.decoder(decoder_in)
-        input = torch.cat([self.embedder_x(im, pos), embed_z], dim=-1)
-        h, c = self.hidden_decoder(input, (h_prev, c_prev))
-        return pos, h, c
+        if self.num_rec_layers == 1:
+            decoder_in = torch.cat([embed_z, h_prev], dim=-1)
+        else:
+            decoder_in = torch.cat([embed_z, h_prev[-1]], dim=-1)
+        x_ = self.decoder(decoder_in)
+        input = torch.cat([self.embedder_x(x_), embed_z], dim=-1)
+        if self.num_rec_layers == 1:
+            h_prev, c_prev = self.hidden_decoder(input, (h_prev, c_prev))
+        else:
+            b, *_ = input.size()
+            for i in range(self.num_rec_layers):
+                if i==0:
+                    h_prev[i], c_prev[i] = self.hidden_decoder[i](input, (h_prev[i], c_prev[i]))
+                else:
+                    h_prev[i], c_prev[i] = self.hidden_decoder[i](h_prev[i-1], (h_prev[i], c_prev[i]))
+        return x_, h_prev, c_prev
 
     def _prior(self, h):
         (z_mu, z_log_var) = self.prior(h).split(self.latent_dim, dim=-1)
         return z_mu, z_log_var
 
     def _sample(self, h):
-        (z_mu, z_log_var) = self._prior(h)
+        if self.num_rec_layers == 1:
+            (z_mu, z_log_var) = self._prior(h)
+        else:
+            (z_mu, z_log_var) = self._prior(h[-1])
         eps = torch.normal(mean=torch.zeros_like(z_mu)).to(z_mu.device)
         # Cap std to 100 for stability
         z_std = (z_log_var*0.5).exp().to(z_mu.device)
         sample = z_mu + z_std*eps
         return sample
     
-    def predict_sequence(self, input, pos=None, seq_len=None):
+    def predict_sequence(self, input, seq_len=None):
         b, T, *_ = input.size()
-        h_prev = torch.zeros((b, self.hidden_dim)).to(input.device)
-        c_prev = torch.zeros((b, self.hidden_dim)).to(input.device)
+        h_prev = [torch.zeros((b, self.hidden_dim)).to(input.device) for _ in range(self.num_rec_layers)]
+        c_prev = [torch.zeros((b, self.hidden_dim)).to(input.device) for _ in range(self.num_rec_layers)]
+        if self.num_rec_layers == 1:
+            h_prev = h_prev[0]
+            c_prev = c_prev[0]
         if seq_len is None:
             seq_len = T
         for i in range(T):
             last_x = input[:,i,:]
-            last_pos = pos[:,i,:]
             # Autoencode
-            z, _, _ = self._inference(last_x, last_pos, h_prev)
-            _, h_prev, c_prev = self._decode(z, last_x, h_prev, c_prev)
+            z, _, _ = self._inference(last_x, h_prev)
+            _, h_prev, c_prev = self._decode(z, h_prev, c_prev)
         
-        pos_shape = [pos.size(0), seq_len, pos.size(2)]
+        pos_shape = [input.size(0), seq_len, input.size(2)]
         reconstr_seq = torch.zeros(pos_shape).to(input.device)
         for i in range(seq_len):
             z_sampled = self._sample(h_prev)
-            pos_hat, h_prev, c_prev = self._decode(z_sampled, last_x, h_prev, c_prev)
+            pos_hat, h_prev, c_prev = self._decode(z_sampled, h_prev, c_prev)
             reconstr_seq[:, i, :] = pos_hat
 
         return reconstr_seq
         
-
-        
-
-    def forward(self, im, pos=None):
-        b, seq_len, *_ = im.size()
-        h_prev = torch.zeros((b, self.hidden_dim)).to(im.device)
-        c_prev = torch.zeros((b, self.hidden_dim)).to(im.device)
-        reconstr_seq = torch.zeros_like(pos).to(im.device)
+    def forward(self, x):
+        b, seq_len, *_ = x.size()
+        h_prev = [torch.zeros((b, self.hidden_dim)).to(x.device) for _ in range(self.num_rec_layers)]
+        c_prev = [torch.zeros((b, self.hidden_dim)).to(x.device) for _ in range(self.num_rec_layers)]
+        if self.num_rec_layers == 1:
+            h_prev = h_prev[0]
+            c_prev = c_prev[0]
+        reconstr_seq = torch.zeros_like(x).to(x.device)
         sizes_z_params = [b, seq_len, 2, self.latent_dim]
-        z_params = torch.zeros(sizes_z_params).to(im.device)
-        z_params_prior = torch.zeros(sizes_z_params).to(im.device)
+        z_params = torch.zeros(sizes_z_params).to(x.device)
+        z_params_prior = torch.zeros(sizes_z_params).to(x.device)
         for i in range(seq_len):
 
-            last_x = im[:,i,:]
-            last_pos = pos[:,i,:]
-
+            last_x = x[:,i,:]
             # Autoencode
-            z, z_mu, z_log_var = self._inference(last_x, last_pos, h_prev)
-            z_mu_prior, z_log_var_prior = self._prior(h_prev)
-            pos_hat, h_prev, c_prev = self._decode(z, last_x, h_prev, c_prev)
+            z, z_mu, z_log_var = self._inference(last_x, h_prev)
+            if self.num_rec_layers == 1:
+                z_mu_prior, z_log_var_prior = self._prior(h_prev)
+            else:
+                z_mu_prior, z_log_var_prior = self._prior(h_prev[-1])
+            x_hat, h_prev, c_prev = self._decode(z, h_prev, c_prev)
 
             # Collect parameters
-            reconstr_seq[:, i, :] = pos_hat
+            reconstr_seq[:, i, :] = x_hat
             z_params[:, i, 0, :] = z_mu
             z_params[:, i, 1, :] = z_log_var
             z_params_prior[:, i, 0, :] = z_mu_prior
--- train_VRNN.py
+++ train_VRNN.py
@@ -26,6 +26,7 @@ parser.add_argument('--epochs', default=500, type=int, metavar='N', help='number
 parser.add_argument('-b', '--batch-size', default=128, type=int,metavar='N', help='mini-batch size (default: 256)')
 parser.add_argument('--beta', default=1, type=float,metavar='N', help='beta VAE param')
 parser.add_argument('--lr', default=1e-4, type=float, metavar='N', help='learning rate')
+parser.add_argument('--hidden_dim', default=32, type=int, metavar='N', help='dimension of hidden space')
 parser.add_argument('--latent_dim', default=32, type=int, metavar='N', help='dimension of latent space')
 parser.add_argument('--seq_len', default=50, type=int, metavar='N', help='length of input sequene')
 parser.add_argument('--load', action='store', type=str, required=False, help='Path from where to load network.')
@@ -50,7 +51,7 @@ def main():
     dl = BouncingBallDataLoader(args.train_root, images=False)
     train_loader = DataLoader(dl, batch_size=args.batch_size, shuffle=True, num_workers=4)
     # Load model
-    vrnn = VRNN(3, 2, args.latent_dim, args.latent_dim, input_type='base').float().to(device)
+    vrnn = VRNN(3, 2, args.hidden_dim, args.latent_dim, num_rec_layers=3, input_type='base').float().to(device)
     print(vrnn)
     if args.load is not None:
         vrnn.load_state_dict(torch.load(args.load)['vrnn'])
@@ -59,7 +60,7 @@ def main():
     # Set up optimizers
     optimizer = Adam(vrnn.parameters(), lr=args.lr)
     gamma = 0.5
-    scheduler = StepLR(optimizer, step_size=20, gamma=gamma)
+    scheduler = StepLR(optimizer, step_size=30, gamma=gamma)
 
     # Train Loop
     vrnn.train()
@@ -67,12 +68,12 @@ def main():
         
         end = time.time()
         for i, sample in enumerate(train_loader, 1):
-            im, pos = sample[0][:,:args.seq_len], sample[1][:,:args.seq_len]
+            _, pos = sample[0][:,:args.seq_len], sample[1][:,:args.seq_len]
             # Forward sample to network
-            var_im = Variable(im.float(), requires_grad=True).to(device)
+            #var_im = Variable(im.float(), requires_grad=True).to(device)
             var_pos = Variable(pos.float(), requires_grad=True).to(device)
             optimizer.zero_grad()
-            reconstr_seq, z_params, z_params_prior = vrnn(var_im, var_pos)
+            reconstr_seq, z_params, z_params_prior = vrnn(var_pos)
             # Compute loss and optimize params
             kld = args.beta*kld_loss(z_params[:,:,0,:], z_params[:,:,1,:], z_params_prior[:,:,0,:], z_params_prior[:,:,1,:])
             mse = F.mse_loss(reconstr_seq, var_pos, reduction='sum')/(args.batch_size)
@@ -94,7 +95,7 @@ def main():
                 writer.add_scalar('data/kl_loss', kld, i + epoch*len(train_loader))
                 writer.add_scalar('data/total_loss', loss, i + epoch*len(train_loader))
                 with torch.no_grad():
-                    pred_pos = vrnn.predict_sequence(var_im, var_pos)
+                    pred_pos = vrnn.predict_sequence(var_pos)
                     target_pos = sample[1][:,args.seq_len:].float().to(pred_pos.device)
                     pred_mse = F.mse_loss(pred_pos, target_pos, reduction='sum')/(args.batch_size)
                     writer.add_scalar('data/prediction_loss', pred_mse, i + epoch*len(train_loader))
