82c84878598de1041933baaba0780d0bc3ceb7c9
Carles Balsells Rodas <carlesbalsellsrodas@hotmail.com>
2022-06-09 10:25:15 +0100

    Jacobian computation time

--- ExtKalman_concept.py
+++ ExtKalman_concept.py
@@ -3,29 +3,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-
-class Network(nn.Module):
-    def __init__(self, num_in, num_out):
-        super().__init__()
-        self.mlp = nn.Linear(num_in, num_out)
-
-    def forward(self, x):
-        return F.softplus(self.mlp(x))
+from models.modules import MLPJacobian
+import time
 
 
 if __name__=="__main__":
     torch.manual_seed(0)
-    net = Network(2, 3)
+    net = MLPJacobian(32, 256, 32).cuda().float()
 
-    B = 4
-    sample = Variable(torch.randn(B,2), requires_grad=True)
+    B = 32
+    sample = Variable(torch.randn(B,32), requires_grad=True).cuda().float()
 
     y = net(sample)
     jac_list = []
+    start = time.time()
     for i in range(B):
         jacob = torch.autograd.functional.jacobian(net, sample[i],
                                                create_graph=True,
                                                vectorize=True)
-        print(jacob)
         jac_list.append(jacob)
-    
+    print("Pytorch jacobian time: ", time.time() - start)
+    start = time.time()
+    y, jacob = net.jacobian(sample)
+    print("Explicit jacobian time: ", time.time() - start)
+    print(y.size())
+    print(jacob.size())
